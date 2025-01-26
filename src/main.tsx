e04b0f321e590189997224e39067e35387cc3375
seungboshim <fodo9898@gmail.com>
2025-01-26 19:02:16 +0900

    gh pages setting

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,20 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import { createHashRouter, RouterProvider } from "react-router-dom";
 
-createRoot(document.getElementById('root')!).render(
+const router = createHashRouter([
+  {
+    path: "/",
+    element: <App />,
+    loader: () => <div>로딩 중..</div>,
+    ErrorBoundary: () => <div>에러 발생~</div>,
+  },
+]);
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <RouterProvider router={router} />
+  </StrictMode>
+);
